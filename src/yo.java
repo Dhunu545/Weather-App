73c2bc22cee4e660ab8e396a1a9e6630872a434f
Dhunu545 <hamidulhaque545@gmail.com>
2024-04-15 09:03:07 +0530

    Deletd some files

--- src/yo.java
+++
@@ -1,9 +0,0 @@
-public class yo {
-    public static void main(String[] args) {
-        String s1 = new String("a");
-        String s2 = "a";
-        String s3 = s2.intern();
-        System.out.println(s1 == s2);
-        System.out.println(s2 == s3);
-    }
-}
